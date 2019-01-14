972a300dd91590e7615680877cec7f117ee96895
cherinlinson <31446244+cherinlinson@users.noreply.github.com>
2019-01-14 16:53:03 -0500

    week2-class work

---
+++ script.js
@@ -0,0 +1,12 @@
+//Name: Cherin Linson
+//Date: January 14,2019
+//Student: C0745072
+//Course: CSD 2214 Section 6
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daySpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
